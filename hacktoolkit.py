465464529cc835c5b9c29b35edf5b7d94cc1839e
mrrobothack <64685128+mrrobothack@users.noreply.github.com>
2020-05-02 14:43:58 +0300

    Mrrobot Hack Tool Kit
    Mrrobot Hack Tool Kit

--- hacktoolkit.py
+++ hacktoolkit.py
@@ -23,31 +23,47 @@ print(banner)
 print("""
 1]Linux Update
 2]Hack Tool Kit Update
-3]Phishing
-4]Aircrack-ng
-5]Exit""")
+-----Phishing-----
+3]Blackeye
+4]Shellphish
+5]Aircrack-ng
+6]Exit""")
 girdi=input("------>")
 
 if (girdi==2):
 	os.system("cd .. && git clone https://github.com/mrrobothack/hacktoolkit.git")
 
+
 if (girdi==3):
 	os.system("clear")
 	print(banner)
 	print("""
-111]ShellPhish
-222]BlackEye""")
-girdiii=input("------>")
-if (girdiii==111):
-	os.system("clear && cd shellphish && bash shellphish.sh")
-if (girdiii==222):
+1111]Kur
+2222]Devam Et""")
+girdi72=input("------>")
+if (girdi72==1111):
+	os.system("clear && git clone https://github.com/thelinuxchoice/blackeye.git && exit")
+if (girdi72==2222):
 	os.system("clear && cd blackeye && bash blackeye.sh")
 
 
-if (girdi==4):
+if (girdi==5):
 	os.system("exit")
 
-if (girdi==5):
+if (girdi==4):
+	os.system("clear")
+	print(banner)
+	print("""
+11111]Kur
+22222]Devam Et""")
+girdi74=input("------>")
+if (girdi74==11111):
+	os.system("clear && git clone https://github.com/thelinuxchoice/shellphish.git && exit")
+if (girdi74==22222):
+	os.system("clear && cd shellphish && bash shellphish.sh")
+
+
+if (girdi==6):
 	os.system("exit")
 
 
@@ -65,6 +81,9 @@ if (girdii==22):
 	os.system("sudo apt-get upgrade")
 if (girdii==33):
 	os.system("sudo apt-get dist-upgrade")
+
+
+
 
 
 
